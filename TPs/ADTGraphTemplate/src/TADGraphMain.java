d1fd0a9f1b3d30b95af592ac1ac6479128ee2267
202000753 <202000753@estudantes.ips.pt>
2021-11-17 17:52:09 +0000

    17-11

--- TPs/ADTGraphTemplate/src/TADGraphMain.java
+++ TPs/ADTGraphTemplate/src/TADGraphMain.java
@@ -8,7 +8,6 @@ public class TADGraphMain {
     public static void main(String[ ] args){
 
         Graph<Local, Bridge> graph = new GraphEdgeList<>();
-    ///Complete
 
         Vertex<Local> localA = graph.insertVertex(new Local("A"));
         Vertex<Local> localB = graph.insertVertex(new Local("B"));
@@ -48,27 +47,114 @@ public class TADGraphMain {
 
 
         System.out.println("\nDFS");
-        Stack<Vertex<Local>> stack = new Stack();
-        stack.push(localA);
-        Set<Vertex<Local>> visited = new HashSet();
-        visited.add(localA);
-        ArrayList list = new ArrayList();
+        System.out.println(dfs(localA, graph));
 
-        while (!stack.empty()) {
-            Vertex<Local> v = (Vertex) stack.pop();
+        System.out.println("\nBFS");
+        System.out.println(bfs(localA, graph));
+
+        System.out.println("\nDijsktra");
+        System.out.println(mini(localA, graph););
+    }
+
+    public static Collection<Vertex<Local>> dfs(Vertex<Local> vertice_root, Graph<Local, Bridge> graph) {
+        HashSet<Vertex<Local>> visited = new HashSet<>();
+        Stack<Vertex<Local>> stack = new Stack<>();
+        ArrayList<Vertex<Local>> list = new ArrayList<>();
+
+        visited.add(vertice_root);
+        stack.push(vertice_root);
+
+        while (!stack.isEmpty()) {
+            Vertex<Local> v = stack.pop();
             list.add(v);
-            Iterator var7 = graph.incidentEdges(v).iterator();
 
-            while (var7.hasNext()) {
-                Edge edge = (Edge) var7.next();
-                Vertex<Local> w = graph.opposite(v, edge);
+            for (Edge<Bridge, Local> e:graph.incidentEdges(v)){
+                Vertex w = graph.opposite(v, e);
+
                 if (!visited.contains(w)) {
-                    visited.add(w);
                     stack.push(w);
+                    visited.add(w);
                 }
             }
         }
 
-        System.out.println(list);
+        return list;
+    }
+
+    public static Collection<Vertex<Local>> bfs(Vertex<Local> vertice_root, Graph<Local, Bridge> graph) {
+        HashSet<Vertex<Local>> visited = new HashSet<>();
+        Queue<Vertex<Local>> queue = new LinkedList<>();
+        ArrayList<Vertex<Local>> list = new ArrayList<>();
+
+        visited.add(vertice_root);
+        queue.offer(vertice_root);
+
+        while (!queue.isEmpty()) {
+            Vertex<Local> v = queue.poll();
+            list.add(v);
+
+            for (Edge<Bridge, Local> e:graph.incidentEdges(v)){
+                Vertex w = graph.opposite(v, e);
+
+                if (!visited.contains(w)) {
+                    queue.offer(w);
+                    visited.add(w);
+                }
+            }
+        }
+
+        return list;
+    }
+
+    private void dijkstra(Vertex<Local> orig, Map<Vertex<Local>, Double> costs, Map<Vertex<Local>, Vertex<Local>> predecessors, GraphEdgeList<Local, Bridge> graph) {
+        Set<Vertex<Local>> unvisited = new HashSet<>();
+
+        for (Vertex<Local> v:unvisited)
+            costs.put(v, Double.MAX_VALUE);
+
+        costs.put(orig, 0.0);
+
+        for (Vertex<Local> v:unvisited)
+            predecessors.put(v, null);
+
+        Vertex<Local> u=null;
+
+        while (!unvisited.isEmpty()) {
+            u=findMinimumVertex(unvisited, costs);
+
+            if (costs.get(u)==Double.MAX_VALUE)
+                return;
+
+            unvisited.remove(u);
+
+            for (Edge<Bridge, Local> e:graph.incidentEdges(u)){
+                Vertex w = graph.opposite(u, e);
+                double cost = e.element().getCost() + costs.get(u);
+            }
+        }
+
+    }
+
+    private static Vertex<Local> findMinimumVertex(Set<Vertex<Local>> unvisited, Map<Vertex<Local>, Double> cost) {
+        Vertex<Local> vMin=null;
+        double minCost=Double.MAX_VALUE;
+
+        for (Vertex<Local> v:unvisited) {
+            double minV=cost.get(v);
+
+            if(minV<minCost) {
+                vMin = v;
+                minCost = minV;
+            }
+        }
+
+        return vMin;
+    }
+
+    public int minimumCostPath(Vertex<Local> orig, Vertex<Local> dst, List<Vertex<Local>> localsPath, GraphEdgeList<Local, Bridge> graph) {
+        
+
+
+        return 0;
     }
 }
